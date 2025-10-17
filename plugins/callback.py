ddc4b24b61b624949f4c08347ca6b3296b5f6e6d
UHD Official <uhdnetwork@gmail.com>
2025-10-17 21:27:38 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,3 +1,8 @@
+# ⚡️ Do Not Remove Credit - Made by @UHD_Bots
+# 💬 For Any Help Join Support Group: @UHDBots_Support
+# 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
+
+
 from pyrogram import Client
 from plugins.emojis import EMOJIS
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
