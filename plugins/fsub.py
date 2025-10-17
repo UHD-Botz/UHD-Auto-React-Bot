317a4b84fd2a2c2ad7196711caee59c0bcc6abb1
UHD Official <uhdnetwork@gmail.com>
2025-10-17 21:29:50 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -1,3 +1,9 @@
+# ⚡️ Do Not Remove Credit - Made by @UHD_Bots
+# 💬 For Any Help Join Support Group: @UHDBots_Support
+# 🚫 Removing or Modifying these Lines will Cause the bot to Stop Working.
+
+
+
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import AUTH_CHANNELS
 from pyrogram import Client
